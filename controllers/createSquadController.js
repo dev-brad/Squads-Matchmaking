279ff220e468e0a12ec51aec24898af7a5187bfd
Brad Griffith <brad.s.griffith@gmail.com>
2021-04-22 21:28:06 -0400

    add squads request to multiple users

--- controllers/createSquadController.js
+++ controllers/createSquadController.js
@@ -2,14 +2,12 @@ const mongoose = require('mongoose');
 const Squads = require(__dirname + './../models/team-model');
 const crud = require(__dirname + "./../crud.js");
 
-exports.createNewSquad = async function (req, res) {
-    let email = req.session.email;
-    let teamMates = req.body.player;
+// exports.createNewSquad = async function (req, res) {
+//     let email = req.session.email;
+//     let teamMates = req.body.player;
 
-    let user = await crud.findSquadsName(email);
-    teamMates.push(user);
-
-
+//     let user = await crud.findSquadsName(email);
+//     teamMates.push(user);
 
 //     const newSquad = new Squads({
 //         teamLeaderEmail: req.session.email,
@@ -29,7 +27,7 @@ exports.createNewSquad = async function (req, res) {
 //             res.status(500).json({ error: err });
 //         });
 // }
-};
+// };
 
 exports.get_friends = async function (req, res) {
 
@@ -51,17 +49,22 @@ exports.post_team_request = async function(req, res) {
 
     if (req.isAuthenticated()){
 
-        let teamMates = [];
-        teamMates.push(req.body.player);
+        let teamMates;
+        if (Array.isArray(req.body.player)) {
+            teamMates = req.body.player;
+        } else {
+            teamMates = [];
+            teamMates.push(req.body.player);
+        }
+        
         let email = req.session.email;
         let squadsName = await crud.findSquadsName(email);
 
         if (teamMates.length > 0) {
-            console.log(teamMates);
             for (var i = 0; i < teamMates.length; i++) {
-                console.log(teamMates[i]);
+                
                 let user = await crud.checkSquadsUsernameExists(teamMates[i]);
-                console.log(user);
+
                 if (user.email != email) {
                     await crud.createNewTeamRequestDocument(user.email, user.squadsName, req.body.sname, req.body.gameType, email, squadsName);
                 }
